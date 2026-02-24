9a8ebb1445e92343f459f97a09966db1727ffa1e
Vivek Rawat <vvk051976@gmail.com>
2026-02-24 15:29:08 +0530

    Add files via upload

---
+++ DAY9-Q1.c
@@ -0,0 +1,26 @@
+/*Problem: A secret system stores code names in forward order. To display them in mirror format, you must transform the given code name so that its characters appear in the opposite order.
+
+Input:
+- Single line: a lowercase string containing only alphabetic characters (no spaces)
+
+Output:
+- Print the transformed code name after applying the mirror operation
+
+Example:
+Input:
+hello
+
+Output:
+olleh
+
+Explanation: The first character moves to the last position, the second to the second-last, and so on until the entire string is mirrored*/
+#include<stdio.h>
+#include<string.h>
+int main(){
+char s[100];
+scanf("%s",s);
+int i,l=strlen(s);
+for(i=l-1;i>=0;i--)
+printf("%c",s[i]);
+return 0;
+}
